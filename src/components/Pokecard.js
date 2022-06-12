cce0ec0d182f56791da3dfed5c23773b7eea86b6
Asad Mahmood <asadmhmd1234@gmail.com>
2022-06-13 02:06:17 +0530

    Add styles to the pokecard component

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -1,14 +1,18 @@
 import React from "react";
+import "./Pokecard.css";
 
 const Pokecard = (props) => {
   function handlePokemonClick() {
     props.clickHandler(props.id);
   }
   return (
-    <div onClick={handlePokemonClick}>
-      <img src={props.image} alt={props.name} />
-      <br />
-      {props.name[0].toUpperCase() + props.name.slice(1)}
+    <div className="pokecard" onClick={handlePokemonClick}>
+      <div className="image">
+        <img src={props.image} alt={props.name} />
+      </div>
+      <div className="name">
+        {props.name[0].toUpperCase() + props.name.slice(1)}
+      </div>
     </div>
   );
 };
